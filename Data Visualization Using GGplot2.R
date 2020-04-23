ac2ef3aa4b09081d195f4a3b06cdfbfe91ea9c15
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-04-23 15:30:37 -0700

    2020.04.23 1527PST using group by and summarize

--- Data Visualization Using GGplot2.R
+++ Data Visualization Using GGplot2.R
@@ -29,3 +29,80 @@
 ##Libraries
 #*********************************
 library(ggplot2)
+library(ggplot2)
+theme_set(
+  theme_classic() +
+    theme(legend.position = "top")
+)
+library(dplyr)
+
+#*********************************
+## 1.) bringing in the data & checking dimensions
+#*********************************
+
+df <- ToothGrowth
+
+dim(ToothGrowth)
+# Expected Results
+# > dim(ToothGrowth)
+# [1] 60  3
+
+df$dose <- as.factor(df$dose)
+head(df,3)
+
+#*********************************
+## 2.) Summary Statistics
+#*********************************
+# Direction: Compute summary statistics for the variable len 
+#            organized into groups by the variable dose:
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - | 
+#   Group by                                        |
+#   Group by one or more variables - Description    |
+#                                                   |
+# Most data operations are done on groups defined by|
+# variables. group_by() takes an existing tbl and   |
+# converts it into a grouped tbl where operations   |
+# are performed "by group".                         |
+# ungroup() removes grouping                        |
+# - - - - - - - - - - - - - - - - - - - - - - - - - |
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - | 
+#   Summarise                                       |
+#   summarise {dplyr}                               |
+#   Reduce multiple values down to a single value   |
+#                                                   |
+#   Description                                     |
+#                                                   |
+# Create one or more scalar variables summarizing   |
+# the variables of an existing tbl. Tbls with       |
+# groups created by group_by() will result in one   |
+# row in the output for each group. Tbls with no    |
+# groups will result in one row.                    |
+#                                                   |
+# - - - - - - - - - - - - - - - - - - - - - - - - - |
+
+df.summary <- df %>%
+  group_by(dose) %>%
+  summarise(
+    sd = sd(len, na.rm = TRUE),
+    len = mean(len)
+  )
+df.summary
+
+
+# Results
+# A tibble: 3 x 3
+#   dose     sd   len
+#   <fct> <dbl> <dbl>
+#[1] 0.5    4.50  10.6
+#[2] 1      4.42  19.7
+#[3] 2      3.77  26.1
+
+
+
+
+
+
+
