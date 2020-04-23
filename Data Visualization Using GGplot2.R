e420a6ec515b63e270a92f52565244492f87aad4
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-04-23 15:09:37 -0700

    2020.04.23 1506PST created and saved to go along with tutorial

---
+++ Data Visualization Using GGplot2.R
@@ -0,0 +1,31 @@
+##Hello World
+
+## Author: https://www.datanovia.com/
+## Notes: Kate Rovinski
+## Title: DATA VISUALIZATION USING GGPLOT2
+## Date: April 2020
+
+# R script below follows the tutorial on how to assign error bars in GGplot
+
+# Error Bars are used to visualize the variability of the plotted data. 
+# Error Bars can be applied to graphs such as, 
+#                                           Dot Plots, 
+#                                           Barplots, or 
+#                                           Line Graphs 
+#                             to provide an additional layer of detail on the presented data.
+
+#Generally, Error bars are used to show either the standard deviation, 
+#                             standard error, confidence intervals or interquartile range.
+
+# The length of an Error Bar helps reveal the uncertainty of a data point: 
+#                             a short Error Bar shows that values are concentrated, 
+#                             signalling that the plotted average value is more likely, 
+#                             while a long Error Bar would indicate that the values 
+#                                            are more spread out and less reliable.
+
+
+
+#*********************************
+##Libraries
+#*********************************
+library(ggplot2)
